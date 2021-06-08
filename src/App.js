432bbf79519d530a2fbfcaf822e6ffa4d0083c95
Abdur rahman <abdur.rahman200301@gmail.com>
2021-06-09 00:43:50 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import ImageCard from "./Components/ImageCard";
+import Search from "./Components/Search";
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
+  return (
+    <div className="container mx-auto">
+      <Search searchText={(text) => setTerm(text)} />
+      {isLoading ? (
+        <h3 className="text-center mr-auto text-7xl">Loading</h3>
+      ) : (
+        <div className="grid grid-cols-5 gap-2 " >
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/ImageCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const ImageCard = ({ image }) => {
+  const { user, webformatURL, downloads, views, likes } = image;
+  const tags = image.tags.split(",");
+  return (
+    <>
+      <div className="max-w-sm overflow-hidden rounded-sm shadow-lg bg-blend-color">
+        <img src={webformatURL} alt="random" />
+        <h2 className="font-bold text-purple-500 my-3"> {user}</h2>
+
+        <ul>
+          <li>
+            <strong> Views: </strong>
+            {views}
+          </li>
+          <li>
+            <strong> downloads: </strong>
+            {downloads}
+          </li>
+          <li>
+            <strong> Likes: </strong>
+            {likes}
+          </li>
+        </ul>
+        <div className="px-6 py-4">
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 my-2"
+            >
+              #{tag}
+            </span>
+          ))}
+        </div>
+        <button className="bg-red-800 hover:bg-red-600 text-white w-full font-bold py-3 px-4 rounded">
+          Details
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default ImageCard;
